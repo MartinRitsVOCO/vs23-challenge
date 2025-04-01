ea1ff6178f37fcfcef144dc24ea61dcce7b56ce7
MartinRitsVOCO <martin.ritsberg@voco.ee>
2025-04-01 09:39:17 +0300

    Ülesanne 3 - andmete pärimine Meals.js komponendis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+
 const Meals = () => {
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        async function fetchMeals() {
+            fetch("http://localhost:3001/meals")
+            .then(response => response.json())
+            .then(data => setMeals(data))
+        }
+
+        fetchMeals()
+    }, [])
+
+    console.log(meals)
+
     return (
         <ul id="meals">
             { 
